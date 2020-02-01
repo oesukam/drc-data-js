da4c578cc56a590fcaca8df9466fd1b9a9053811
Olivier <oesukam@gmail.com>
2020-02-01 16:41:29 +0200

    setup jest test

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  clearMocks: true,
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: ['src/**/*.{js,ts}'],
+  coveragePathIgnorePatterns: [
+    'node_modules',
+    'coverage',
+    'dist',
+  ],
+  verbose: true,
+  coverageThreshold: {
+    global: {
+      functions: 0,
+      lines: 0,
+      statements: -10,
+    },
+  },
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  testMatch: ['**/__tests__/**/*.{js,ts}?(x)'],
+};
