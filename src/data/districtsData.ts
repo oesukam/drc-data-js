4f91d5863bbacaac79b389f345543339f878f3aa
Olivier <oesukam@gmail.com>
2020-02-02 15:31:02 +0200

    remove unnessary scripts

---
+++ src/data/districtsData.ts
@@ -0,0 +1,108 @@
+const provincesData = {
+  'bas-uele': {
+    name: 'Bas-Uele',
+    p_code: 'CD52',
+  },
+  equateur: {
+    name: 'Équateur',
+    p_code: 'CD41',
+  },
+  'haut-katanga': {
+    name: 'Haut-Katanga',
+    p_code: 'CD71',
+  },
+  'haut-lomami': {
+    name: 'Haut-Lomami',
+    p_code: 'CD73',
+  },
+  'Haut-Uele': {
+    name: 'Haut-Uele',
+    p_code: 'CD53',
+  },
+  ituri: {
+    name: 'Ituri',
+    p_code: 'CD54',
+  },
+  kasai: {
+    name: 'Kasaï',
+    p_code: 'CD92',
+  },
+  'kasai-central': {
+    name: 'Kasaï-Central',
+    p_code: 'CD91',
+  },
+  'kasai-oriental': {
+    name: 'Kasaï-Oriental',
+    p_code: 'CD82',
+  },
+  kinshasa: {
+    name: 'Kinshasa',
+    p_code: 'CD10',
+  },
+  'kongo-central': {
+    name: 'Kongo-Central',
+    p_code: 'CD20',
+  },
+  kwango: {
+    name: 'Kwango',
+    p_code: 'CD31',
+  },
+  kwilu: {
+    name: 'Kwilu',
+    p_code: 'CD32',
+  },
+  lomami: {
+    name: 'Lomami',
+    p_code: 'CD81',
+  },
+  lualaba: {
+    name: 'Lualaba',
+    p_code: 'CD72',
+  },
+  'mai-ndombe': {
+    name: 'Maï-Ndombe',
+    p_code: 'CD33',
+  },
+  maniema: {
+    name: 'Maniema',
+    p_code: 'CD63',
+  },
+  mongala: {
+    name: 'Mongala',
+    p_code: 'CD44',
+  },
+  'nord-Kivu': {
+    name: 'Nord-Kivu',
+    p_code: 'CD61',
+  },
+  'nord-ubangi': {
+    name: 'Nord-Ubangi',
+    p_code: 'CD43',
+  },
+  sankuru: {
+    name: 'Sankuru',
+    p_code: 'CD83',
+  },
+  'sud-kivu': {
+    name: 'Sud-Kivu',
+    p_code: 'CD62',
+  },
+  'sud-ubangi': {
+    name: 'Sud-Ubangi',
+    p_code: 'CD42',
+  },
+  tanganyika: {
+    name: 'Tanganyika',
+    p_code: 'CD74',
+  },
+  tshopo: {
+    name: 'Tshopo',
+    p_code: 'CD51',
+  },
+  tshuapa: {
+    name: 'Tshuapa',
+    p_code: 'CD45',
+  },
+};
+
+export default provincesData;
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import provincesFunc from './provinces';
+import territoriesFunc from './territories';
 
 export const provinces = provincesFunc;
+export const territories = territoriesFunc;
 
 const drc = {
   provinces,
+  territories,
 };
 
 export default drc;
